f215c25ab2884bbb7138483fbdd24447b701ab76
V0000DY <proektant.burenie@gmail.com>
2025-05-07 13:47:42 +0700

    svg loader, file loader, button ui kit, dark theme switcher

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,11 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -31,6 +36,15 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: "ts-loader",
     exclude: /node_modules/,
   };
+  // Лоадер для файлов - иззначально для png, jpg, jpeg, gif, затем добавили шрифты
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
 
-  return [typescriptLoader, cssLoaders];
+  return [fileLoader, svgLoader, typescriptLoader, cssLoaders];
 }
