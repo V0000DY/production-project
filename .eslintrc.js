8b0fd76cf966b04e5d498a0168cb7ddab987bef1
V0000DY <proektant.burenie@gmail.com>
2025-05-14 18:08:43 +0700

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: ["plugin:react/recommended", "airbnb", "plugin:i18next/recommended"],
   parser: "@typescript-eslint/parser",
@@ -33,6 +34,7 @@ module.exports = {
     "comma-dangle": "off",
     "object-curly-newline": "off",
     "i18next/no-literal-string": ["error", { markupOnly: true }],
+    "max-len": ["error", { ignoreComments: true }],
   },
   globals: {
     __IS_DEV__: true,
