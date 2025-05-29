a874394aa2ad3f7859e1dc464fcba8139c0f49c8
V0000DY <proektant.burenie@gmail.com>
2025-05-29 13:22:50 +0700

    add stories for input and login form

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,14 +1,8 @@
-import { Counter } from "entities/Counter";
 import { useTranslation } from "react-i18next";
 
 const AboutPage = () => {
   const { t } = useTranslation();
-  return (
-    <div>
-      {t("about")}
-      <Counter />
-    </div>
-  );
+  return <div>{t("about")}</div>;
 };
 
 export default AboutPage;
