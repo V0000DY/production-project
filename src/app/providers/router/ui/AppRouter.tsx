1d868ebbc1cee90fe2553aebf82593bfa91f32a5
V0000DY <proektant.burenie@gmail.com>
2025-06-27 13:45:42 +0700

    updaate app router

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,34 +1,30 @@
 import { Route, Routes } from "react-router-dom";
-import { memo, Suspense, useMemo } from "react";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { memo, Suspense, useCallback } from "react";
+import {
+  AppRouteProps,
+  routeConfig,
+} from "shared/config/routeConfig/routeConfig";
 import { PageLoader } from "widgets/PageLoader";
-import { useSelector } from "react-redux";
-import { getUserAuthData } from "entities/User";
+import RequireAuth from "./RequireAuth";
 
 const AppRouter = () => {
-  const isAuth = useSelector(getUserAuthData);
-
-  const routes = useMemo(
-    () =>
-      Object.values(routeConfig).filter(
-        (route) => !(route.authOnly && !isAuth) // Если пользователь не авторизован, то фильтруем authOnly
-      ),
-    [isAuth]
-  );
-
-  return (
-    <Suspense fallback={<PageLoader />}>
-      <Routes>
-        {routes.map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={<div className="page-wrapper">{element}</div>}
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
+  const renderWithWrapper = useCallback((route: AppRouteProps) => {
+    const element = (
+      <Suspense fallback={<PageLoader />}>
+        <div className="page-wrapper">{route.element}</div>
+      </Suspense>
+    );
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        element={
+          route.authOnly ? <RequireAuth>{element}</RequireAuth> : element
+        }
+      />
+    );
+  }, []);
+  return <Routes>{Object.values(routeConfig).map(renderWithWrapper)}</Routes>;
 };
 
 export default memo(AppRouter);
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
-export { getUserAuthData } from "./model/selectors/getUserAuthData";
+export { getUserInited } from "./model/selectors/getUserInited/getUserInited";
+export { getUserAuthData } from "./model/selectors/getUserAuthData/getUserAuthData";
 export { UserSchema, User } from "./model/types/user";
 export { userReducer, userActions } from "./model/slice/userSlice";
