bc9fe8ad560a2ea6f56c8a5fa0c2f61ad302d7c4
V0000DY <proektant.burenie@gmail.com>
2025-08-16 14:19:24 +0700

    add private route with required roles

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -1,11 +1,25 @@
-import { getUserAuthData } from "entities/User";
+import { getUserAuthData, getUserRoles, UserRole } from "entities/User";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 import { RoutePath } from "shared/config/routeConfig/routeConfig";
 
-function RequireAuth({ children }: { children: JSX.Element }) {
+interface RequireAuthProps {
+  children: JSX.Element;
+  roles?: UserRole[];
+}
+
+function RequireAuth({ children, roles }: RequireAuthProps) {
   const auth = useSelector(getUserAuthData);
   const location = useLocation();
+  const userRoles = useSelector(getUserRoles);
+
+  const hasRequiredRoles = useMemo(() => {
+    if (!roles) {
+      return true;
+    }
+    return roles.some((requiredRole) => userRoles?.includes(requiredRole));
+  }, [roles, userRoles]);
 
   if (!auth) {
     // Redirect them to the /main page, but save the current location they were
@@ -13,6 +27,12 @@ function RequireAuth({ children }: { children: JSX.Element }) {
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
     return <Navigate to={RoutePath.main} state={{ from: location }} replace />;
+  }
+
+  if (!hasRequiredRoles) {
+    return (
+      <Navigate to={RoutePath.forbidden} state={{ from: location }} replace />
+    );
   }
 
   return children;
---
+++ src/pages/AdminPanelPage/ui/AdminPanelPage.tsx
@@ -0,0 +1,10 @@
+import { memo } from "react";
+import { useTranslation } from "react-i18next";
+import { Page } from "widgets/Page/Page";
+
+const AdminPanelPage = memo(() => {
+  const { t } = useTranslation();
+  return <Page>{t("Admin panel")}</Page>;
+});
+
+export default AdminPanelPage;
