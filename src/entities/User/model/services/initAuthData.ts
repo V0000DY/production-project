19b9135934fa8eb463d9011699aae53d839e5d93
V0000DY <proektant.burenie@gmail.com>
2026-01-12 17:22:12 +0700

    add loading layout, fallback theme, default features

--- src/entities/User/model/services/initAuthData.ts
+++ src/entities/User/model/services/initAuthData.ts
@@ -1,7 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { ThunkConfig } from "@/app/providers/StoreProvider";
 import { User } from "../types/user";
-import { USER_LOCALSTORAGE_KEY } from "@/shared/const/localStorage";
+import {
+  LOCAL_STORAGE_LAST_DESIGN_KEY,
+  USER_LOCALSTORAGE_KEY,
+} from "@/shared/const/localStorage";
 import { getUserDataByIdQuery } from "../../api/userApi";
 
 export const initAuthData = createAsyncThunk<User, void, ThunkConfig<string>>(
@@ -21,6 +24,11 @@ export const initAuthData = createAsyncThunk<User, void, ThunkConfig<string>>(
       if (!response.jsonSettings) {
         return rejectWithValue("");
       }
+
+      localStorage.setItem(
+        LOCAL_STORAGE_LAST_DESIGN_KEY,
+        response.features?.isAppRedesigned ? "new" : "old",
+      );
 
       return response;
     } catch (error) {
--- src/shared/lib/features/lib/setGetFeatures.ts
+++ src/shared/lib/features/lib/setGetFeatures.ts
@@ -1,7 +1,14 @@
+import { LOCAL_STORAGE_LAST_DESIGN_KEY } from "@/shared/const/localStorage";
 import { FeatureFlags } from "@/shared/types/featureFlags";
 
+const defaultFeatures: FeatureFlags = {
+  isAppRedesigned:
+    localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY) === "new",
+};
 // ФИЧИ НЕ МЕНЯЮТСЯ В ХОДЕ СЕССИИ, ИХ НЕОБЯЗАТЕЛЬНО ДЕЛАТЬ РЕАКТИВНЫМИ!
-let featureFlags: FeatureFlags = {};
+let featureFlags: FeatureFlags = {
+  ...defaultFeatures,
+};
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
   if (newFeatureFlags) {
